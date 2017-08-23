7c102e20b9b411ae75288a14cd1a77b9e9ba8874
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-08-23 00:26:57 -0400

    trying different folder structure

--- src/component-anchor.js
+++ src/component-anchor.js
@@ -84,9 +84,9 @@ AFRAME.registerComponent('arjs-anchor', {
 				arProfile.defaultMarkerParameters.type = 'barcode'
 				arProfile.defaultMarkerParameters.barcodeValue = 1001	
 				arProfile.defaultMarkerParameters.markersAreaEnabled = true
-			}else if(_this.data.preset=='custom'){
+			}else if( _this.data.preset == 'custom'){
 				arProfile.defaultMarkerParameters.type = 'pattern'
-				arProfile.defaultMarkerParameters.patternUrl = _this.data.patternUrl;
+				arProfile.defaultMarkerParameters.patternUrl = _this.data.patternUrl
 				arProfile.defaultMarkerParameters.markersAreaEnabled = false
 			}
 			else {
